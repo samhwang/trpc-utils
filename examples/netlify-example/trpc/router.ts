ccd6d979f1c1224ca15da0b616406f1e05cd315e
Sam Huynh <sam@hungryhungry.com>
2023-03-08 09:53:52 +1100

    feat: add some other routers into the example

--- examples/netlify-example/trpc/router.ts
+++ examples/netlify-example/trpc/router.ts
@@ -1,9 +1,24 @@
 import { initTRPC } from '@trpc/server';
+import { z } from 'zod';
 
 export const t = initTRPC.create();
 
 export const userRouter = t.router({
   me: t.procedure.query((_req) => "hello, it's me"),
+  hello: t.procedure
+    .input(
+      z.object({
+        name: z.string(),
+      })
+    )
+    .query(({ input }) => `hello ${input.name}`),
+  changeName: t.procedure
+    .input(
+      z.object({
+        name: z.string(),
+      })
+    )
+    .mutation(({ input }) => ({ name: `Mutated ${input.name}` })),
 });
 
 export const appRouter = t.router({
